39c520d6988a4e3e0827c6a0343ae736f5825778
Vishal Nagda <vishalnagda1@gmail.com>
2020-06-30 19:30:21 +0530

    create an user model

---
+++ app/apis/user/modal.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const modelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+  },
+  {
+    timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
+  }
+);
+
+export default mongoose.model("User", modelSchema);
