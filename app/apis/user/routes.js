bacb46d413b86faac191add0b36643aa4bb0c84d
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-01 01:05:52 +0530

    create a route for user signup

--- app/apis/user/routes.js
+++ app/apis/user/routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("./controller");
 
+router.post("/signup", controller.signup);
 router.post("/signin", controller.signin);
 
 module.exports = router;
