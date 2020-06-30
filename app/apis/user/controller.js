adab40e5cf00a8eb6b4c58a7f52afb0bc34d3cba
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-01 01:08:33 +0530

    validate signup body data

--- app/apis/user/controller.js
+++ app/apis/user/controller.js
@@ -1,9 +1,22 @@
-/**
- * Get contained user using there credentials.
- * @returns {User}
- */
+const isEmpty = require("is-empty");
+const validator = require("./validator");
+const { errorParser } = require("./helper");
+
+// Register a new user
+function signup(request, response) {
+  const { body } = request;
+  const validData = validator.signup.validate({ body }, { abortEarly: false });
+
+  if (!isEmpty(validData.error)) {
+    return response.status(400).json(errorParser(validData.error));
+  }
+
+  return response.send(request.body);
+}
+
+// User login
 function signin(request, response) {
   response.send(request.body);
 }
 
-module.exports = { signin };
+module.exports = { signin, signup };
