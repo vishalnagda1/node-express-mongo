c5d650c8b86d8b23ccd28666057c78ff14ea5261
Vishal Nagda <vishalnagda1@gmail.com>
2020-06-30 16:52:34 +0530

    configure server to connect with mongo db

---
+++ config/keys.sample.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://username:password@localhost:27017/database-name?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const routes = require("./routes");
+
+// DB Config
+const mongoURI = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.info("MongoDB successfully connected"))
+  .catch((err) => console.error(err));
 
 const app = express();
 
 app.use(routes);
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.info(`Server up and running on port ${port} !`));
