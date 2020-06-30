1a25db48e68f2b187c45933714d4b03a31a82301
Vishal Nagda <vishalnagda1@gmail.com>
2020-06-30 19:28:44 +0530

    update server file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const routes = require("./routes");
 
@@ -12,8 +13,14 @@ mongoose
   .then(() => console.info("MongoDB successfully connected"))
   .catch((err) => console.error(err));
 
+mongoose.set("debug", true);
+
 const app = express();
 
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+
+// mounting app routes
 app.use(routes);
 
 const port = process.env.PORT || 8000;
