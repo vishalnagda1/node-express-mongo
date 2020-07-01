412fb17fd3ab62efe1b6d0082a9fd4f7c833bbdd
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-01 16:42:42 +0530

    update validation for user signup

--- app/apis/user/validator.js
+++ app/apis/user/validator.js
@@ -3,8 +3,19 @@ const Joi = require("@hapi/joi");
 module.exports = {
   signup: Joi.object({
     body: Joi.object({
+      name: Joi.string().required(),
       email: Joi.string().email().required(),
       password: Joi.string().length(4).required(),
+      confirm_password: Joi.string()
+        .required()
+        .valid(Joi.ref("password"))
+        .messages({ "any.only": "password does not match" }),
+      mobile_number: Joi.string()
+        .min(8)
+        .max(10)
+        .regex(/^[0-9]+$/)
+        .required()
+        .messages({ "string.pattern.base": "Invalid mobile number" }),
     }),
   }),
 };
