78c3213f2fb11b9c43c64a643e4493ebca52524a
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-01 19:25:42 +0530

    refactor the signup code

--- app/apis/user/controller.js
+++ app/apis/user/controller.js
@@ -7,38 +7,41 @@ const { errorParser } = require("./helper");
 const User = require("./modal");
 
 // Register a new user
-function signup(request, response) {
-  const { body } = request;
-  const validData = validator.signup.validate({ body }, { abortEarly: false });
+async function signup(request, response) {
+  try {
+    const { body } = request;
 
-  if (!isEmpty(validData.error)) {
-    return response.status(400).json(errorParser(validData.error));
-  }
+    // validate data
+    const { error } = validator.signup.validate(
+      { body },
+      { abortEarly: false }
+    );
 
-  User.findOne({ email: body.email }).then((user) => {
-    if (user) {
-      return response.status(400).json({ email: "Email already exists" });
-    } else {
-      const newUser = new User({
-        name: body.name,
-        email: body.email,
-        password: body.password,
-        mobile_number: body.mobile_number,
-      });
-
-      // Hash password before saving in database
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then((user) => response.json(user))
-            .catch((err) => response.status(400).json(err));
-        });
-      });
+    if (error) {
+      return response.status(400).json(errorParser(error));
     }
-  });
+
+    // Check if user exists
+    if (await User.findOne({ email: body.email })) {
+      return response.status(400).json({ email: "Email already exists" });
+    }
+
+    // Creating a new user object
+    const newUser = new User({
+      name: body.name,
+      email: body.email,
+      password: body.password,
+      mobile_number: body.mobile_number,
+    });
+
+    // Hash password before saving in database
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(newUser.password, salt);
+    const user = await newUser.save();
+    return response.json(user);
+  } catch (err) {
+    return response.status(422).json(err);
+  }
 }
 
 // User login
