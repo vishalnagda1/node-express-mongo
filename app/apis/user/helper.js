a813b60f18111ef41052086b3ff9cfdeb00b783e
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-01 16:39:04 +0530

    sanitize error data in helper

--- app/apis/user/helper.js
+++ app/apis/user/helper.js
@@ -3,7 +3,7 @@ const errorParser = (error) => {
   let errorObj = {};
   error.details.map((error) => {
     const { context, message } = error;
-    errorObj[context.key] = message.replace(`"${context.label}"`, context.key);
+    errorObj[context.key] = message.replace(`"${context.label}"`, context.key.replace(/_|-/g, ' '));
   });
   return errorObj;
 };
