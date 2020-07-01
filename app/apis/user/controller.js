a2b66b176decacda3316defbfde5866b3a62182b
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-02 00:51:21 +0530

    refactor the user signup api

--- app/apis/user/controller.js
+++ app/apis/user/controller.js
@@ -1,10 +1,7 @@
-const isEmpty = require("is-empty");
 const bcrypt = require("bcryptjs");
 const validator = require("./validator");
 const { errorParser } = require("./helper");
-
-// Load user model
-const User = require("./modal");
+const { findUserByEmail, createUser } = require("./services");
 
 // Register a new user
 async function signup(request, response) {
@@ -22,23 +19,16 @@ async function signup(request, response) {
     }
 
     // Check if user exists
-    if (await User.findOne({ email: body.email })) {
-      return response.status(400).json({ email: "Email already exists" });
+    if (await findUserByEmail(body.email)) {
+      return response.status(409).json({ email: "Email already exists" });
     }
-
-    // Creating a new user object
-    const newUser = new User({
-      name: body.name,
-      email: body.email,
-      password: body.password,
-      mobile_number: body.mobile_number,
-    });
 
     // Hash password before saving in database
     const salt = await bcrypt.genSalt(10);
-    newUser.password = await bcrypt.hash(newUser.password, salt);
-    const user = await newUser.save();
-    return response.json(user);
+    body.password = await bcrypt.hash(body.password, salt);
+
+    // Creating a new user and return
+    return response.json(await createUser(body));
   } catch (err) {
     return response.status(422).json(err);
   }
