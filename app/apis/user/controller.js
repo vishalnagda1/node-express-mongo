e2aa430b79a6b8f191ad23d68526a3ee7412b1a5
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-01 18:33:54 +0530

    create a functional user signup api

--- app/apis/user/controller.js
+++ app/apis/user/controller.js
@@ -1,6 +1,10 @@
 const isEmpty = require("is-empty");
+const bcrypt = require("bcryptjs");
 const validator = require("./validator");
 const { errorParser } = require("./helper");
+
+// Load user model
+const User = require("./modal");
 
 // Register a new user
 function signup(request, response) {
@@ -11,7 +15,30 @@ function signup(request, response) {
     return response.status(400).json(errorParser(validData.error));
   }
 
-  return response.send(request.body);
+  User.findOne({ email: body.email }).then((user) => {
+    if (user) {
+      return response.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: body.name,
+        email: body.email,
+        password: body.password,
+        mobile_number: body.mobile_number,
+      });
+
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => response.json(user))
+            .catch((err) => response.status(400).json(err));
+        });
+      });
+    }
+  });
 }
 
 // User login
