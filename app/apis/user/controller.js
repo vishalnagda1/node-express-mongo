dbe12af9795a433a2b94fedc166a82bdf190a860
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-02 11:05:52 +0530

    implement the user signin api

--- app/apis/user/controller.js
+++ app/apis/user/controller.js
@@ -1,7 +1,8 @@
 const bcrypt = require("bcryptjs");
 const validator = require("./validator");
-const { errorParser } = require("./helper");
+const { errorParser, signJWT } = require("./helper");
 const { findUserByEmail, createUser } = require("./services");
+const { use } = require("./routes");
 
 // Register a new user
 async function signup(request, response) {
@@ -20,7 +21,7 @@ async function signup(request, response) {
 
     // Check if user exists
     if (await findUserByEmail(body.email)) {
-      return response.status(409).json({ email: "Email already exists" });
+      return response.status(409).json({ email: "email already exists" });
     }
 
     // Hash password before saving in database
@@ -35,8 +36,44 @@ async function signup(request, response) {
 }
 
 // User login
-function signin(request, response) {
-  response.send(request.body);
+async function signin(request, response) {
+  try {
+    const { body } = request;
+
+    // validate data
+    const { error } = validator.signin.validate(
+      { body },
+      { abortEarly: false }
+    );
+
+    if (error) {
+      return response.status(400).json(errorParser(error));
+    }
+
+    // Find the user
+    const user = await findUserByEmail(body.email);
+
+    // Check if user exists
+    if (!user) {
+      return response.status(404).json({ email: "email not found" });
+    }
+
+    // Check password
+    if (!bcrypt.compareSync(body.password, user.password)) {
+      return response.status(400).json({ email: "invalid credentials" });
+    }
+
+    // sign JWT and send it in cookie
+    response.cookie("x-access-token", signJWT(user));
+
+    return response.json({
+      name: user.name,
+      email: user.email,
+      mobile_number: user.mobile_number,
+    });
+  } catch (err) {
+    return response.status(422).json(err);
+  }
 }
 
 module.exports = { signin, signup };
