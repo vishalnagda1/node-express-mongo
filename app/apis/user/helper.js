219277e152cea638e423033f85162c283bf9aeee
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-02 11:01:10 +0530

    create jwt sign helper for user login api

--- app/apis/user/helper.js
+++ app/apis/user/helper.js
@@ -1,11 +1,31 @@
+const jwt = require("jsonwebtoken");
+const keys = require("../../../config/keys");
+
 // Parse JOI errors
 const errorParser = (error) => {
   let errorObj = {};
   error.details.map((error) => {
     const { context, message } = error;
-    errorObj[context.key] = message.replace(`"${context.label}"`, context.key.replace(/_|-/g, ' '));
+    errorObj[context.key] = message.replace(
+      `"${context.label}"`,
+      context.key.replace(/_|-/g, " ")
+    );
   });
   return errorObj;
 };
 
-module.exports = { errorParser };
+const signJWT = (user) => {
+  const { issuer, expiry, privateKey } = keys.jwt;
+  const jwtOptions = {
+    issuer: issuer,
+    audience: String(user.id),
+    expiresIn: expiry,
+  };
+  const data = {
+    id: user.id,
+    email: user.email,
+  };
+  return jwt.sign(data, privateKey, jwtOptions);
+};
+
+module.exports = { errorParser, signJWT };
