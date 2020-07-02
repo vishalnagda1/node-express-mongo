2236db2929aee6cce69f9d7f245e49c3cce00878
Vishal Nagda <vishalnagda1@gmail.com>
2020-07-02 12:09:08 +0530

    update the key sample for jwt config

--- config/keys.sample.js
+++ config/keys.sample.js
@@ -1,3 +1,8 @@
 module.exports = {
   mongoURI: "mongodb+srv://username:password@localhost:27017/database-name?retryWrites=true&w=majority",
+  jwt: {
+    issuer: "DZI",
+    privateKey: "somePrivateKey",
+    expiry: "7d"
+  }
 };
